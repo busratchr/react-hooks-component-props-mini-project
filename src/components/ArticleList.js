5c9480e13ed879e0333d797d4763701ffe937138
BUSRA KUCUK <busra3590@gmail.com>
2023-06-25 15:35:48 +0300

    Done

---
+++ src/components/ArticleList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Article from "./Article";
+
+export default function ArticleList(props){
+    const articleList= props.posts.map((article) =>{
+
+        return (
+            <Article
+            key={article.preview}
+            date={article.date}
+            title={article.title}
+            />
+        );
+    });
+    return <main>{articleList}</main>;
+}
